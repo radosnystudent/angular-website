ecc3bae05c20afb0e3b5613663804e546460df6a
radosnystudent <jeremiasz.pauszek@gmail.com>
2023-06-09 21:33:39 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, ComponentsModule],
       declarations: [AppComponent],
     })
   );
