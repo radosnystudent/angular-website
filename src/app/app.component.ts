e7a7d0ed7a03fee203e157b6f911c9b6230c4e93
radosnystudent <jeremiasz.pauszek@gmail.com>
2024-01-20 15:44:12 +0100

    change colors and migrate sass to @use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 export class AppComponent implements OnInit {
   showNavIcon: boolean = false;
   currentLanguage: string = '';
-  themePalette: ThemePalette = undefined;
+  themePalette: ThemePalette = 'primary';
   navLinks = [
     { location: '/main', translation: 'navigation.homepage', icon: 'menu' },
     {
