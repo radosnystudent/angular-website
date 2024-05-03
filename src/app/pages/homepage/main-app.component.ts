53cd99f13daa814968bd298e6fc69c460ea91a77
radosnystudent <jeremiasz.pauszek@gmail.com>
2024-05-03 21:35:43 +0200

    update homepage, remove about me

--- src/app/pages/homepage/main-app.component.ts
+++ src/app/pages/homepage/main-app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from 'src/app/common/pipes';
+import { BubblesComponent, CardComponent } from 'src/app/components';
 
 @Component({
   selector: 'sw-main-app',
   templateUrl: './main-app.component.html',
   standalone: true,
+  imports: [CardComponent, TranslatePipe, BubblesComponent],
 })
-export class MainAppComponent {}
+export class MainAppComponent {
+  image = { path: 'assets/photo.png', alt: 'my photo' };
+}
